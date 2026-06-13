9e4c373fc1beb388bb6e63bb20c67837eb9cf94e
hezronjo48-ux <hezronjo48@gmail.com>
2026-06-13 12:45:41 +0300

    Add PWA offline support - manifest, service worker, icon

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'dmark-v1';
+var STATIC_URLS = [
+  '/css/style.css',
+  '/manifest.json',
+  '/icons/icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(STATIC_URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match('/');
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          if (event.request.url.indexOf(self.location.origin) === 0) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      });
+    })
+  );
+});
