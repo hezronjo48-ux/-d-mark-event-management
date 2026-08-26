62177c60d9099465a561a0bb211a5c65fe0870f8
hezronjo48-ux <hezronjo48@gmail.com>
2026-08-26 15:14:16 +0300

    Fix CSP: add production domains to connectSrc

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(helmet({
       styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com', 'https://cdnjs.cloudflare.com'],
       fontSrc: ["'self'", 'https://fonts.gstatic.com', 'https://cdnjs.cloudflare.com'],
       imgSrc: ["'self'", 'data:', 'https:'],
-      connectSrc: ["'self'"],
+      connectSrc: ["'self'", 'https://dmarkevents.xyz', 'https://dmark-wedding.fr-1.instapods.app'],
       objectSrc: ["'none'"],
       frameAncestors: ["'none'"],
       baseUri: ["'self'"],
